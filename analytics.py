6531ff49d833b9c566be7bca7106aa80db811596
chrpr <corey.harper@gmail.com>
2015-11-23 18:10:31 -0500

    Removing old profile_id code

--- analytics.py
+++ analytics.py
@@ -105,33 +105,6 @@ def main(argv):
     print ('The credentials have been revoked or expired, please re-run '
            'the application to re-authorize')
 
-def get_first_profile_id(service):
-  # Get a list of all Google Analytics accounts for this user
-  accounts = service.management().accounts().list().execute()
-
-  if accounts.get('items'):
-    # Get the first Google Analytics account
-    firstAccountId = accounts.get('items')[0].get('id')
-    #firstAccountId = accounts.get('items')[0].get('id')
-
-    # Get a list of all the Web Properties for the first account
-    webproperties = service.management().webproperties().list(accountId=firstAccountId).execute()
-
-    if webproperties.get('items'):
-      # Get the first Web Property ID
-      firstWebpropertyId = webproperties.get('items')[0].get('id')
-
-      # Get a list of all Views (Profiles) for the first Web Property of the first Account
-      profiles = service.management().profiles().list(
-          accountId=firstAccountId,
-          webPropertyId=firstWebpropertyId).execute()
-
-      if profiles.get('items'):
-        # return the first View (Profile) ID
-        #return profiles.get('items')[0].get('id')
-        return profiles.get('items')[0].get('id')
-  return None
-
 def get_results(service, profile_id):
   # Use the Analytics Service Object to query the Core Reporting API
   return service.data().ga().get(
